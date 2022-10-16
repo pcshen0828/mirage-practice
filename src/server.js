826fb14c99e880ce65f1d3c3f49d4ce43095c934
pcshen0828 <michelleshen0828@gmail.com>
2022-10-16 15:58:48 +0800

    Set up initial data with 'seeds' hook.

--- src/server.js
+++ src/server.js
@@ -7,6 +7,15 @@ export default function makeServer() {
       reminder: Model,
     },
 
+    // seed Mirage with some initial data
+    seeds(server) {
+      // create new reminders in Mirage's data layer
+      // IDs are automatically assigned
+      server.create("reminder", { text: "Walk the dog" });
+      server.create("reminder", { text: "Take out the trash" });
+      server.create("reminder", { text: "Work out" });
+    },
+
     routes() {
       this.get("/api/reminders", (schema) => {
         return schema.reminders.all();
